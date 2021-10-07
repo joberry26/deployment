2266ccd946b65d41cbf13a5e0129909fd2514ed2
Joseph Berry <josephberry910@gmail.com>
2021-10-07 13:26:10 -0600

    attempting integration

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../index.html"));
 });
 
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../main.js'))
+});
+
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
 const port = process.env.PORT || 4545;
 
 
