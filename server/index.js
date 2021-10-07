7f1c2e9c5fec7b2f607ecdc8b55eb111c5cfd572
Joseph Berry <josephberry910@gmail.com>
2021-10-07 14:14:34 -0600

    linking css

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-// app.get('/js', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../main.js'))
-// });
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../style.css'))
+  })
 
 app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
 const port = process.env.PORT || 4545;
